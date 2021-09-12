18aaa07a64f614bec1e8f687c4f06b5836763288
Andre Velasquez <andrethefirst21@gmail.com>
2021-09-11 18:20:32 -0700

    pass pastry deal pricing test

--- Bakery/Models/PierresBakery.cs
+++ Bakery/Models/PierresBakery.cs
@@ -1,6 +1,6 @@
 namespace PierresBakery.Models
 {
-  public class Pastry
+  public class Bread
   {
     public int Amount { get; set; }
     public int Price { get; set; }
@@ -9,6 +9,42 @@ namespace PierresBakery.Models
     {
       Amount = amount;
       Price = price;
+
+    }
+    public int BreadPrice()
+    {
+      if (Amount > 2)
+      {
+        Price = ((Amount / 3) * 10) + ((Amount % 3) *5);
+        return Price;
+
+      }
+      Price = Amount * 5;
+      return Price;
+    }
+  }
+
+  public class Pastry
+  {
+    public int Amount { get; set; }
+    public int Price { get; set; }
+
+    public Pastry(int amount, int price = 0)
+    {
+    Amount = amount;
+    Price = price;
+
+    }
+
+    public int PastryPrice()
+    {
+      if (Amount >= 3)
+      {
+        Price = ((Amount / 3) * 5) + ((Amount % 3) * 2);
+        return Price;
+      }
+      Price = Amount * 2;
+      return Price;
     }
   }
 }
